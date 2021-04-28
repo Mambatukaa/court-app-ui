1c3fce30dd91b8e244696f38da326b79cdce3a86
Bat-Amgalan Nasan-Ochir <batamgalan.n@nmma.co>
2021-04-28 17:32:33 +0800

    schedule delete refactor

--- src/modules/courts/components/CourtDetail.jsx
+++ src/modules/courts/components/CourtDetail.jsx
@@ -8,55 +8,63 @@ const CourtDetail = props => {
   const { courtDetail, addSchedule, deleteSchedule } = props;
   const { courtSchedule } = courtDetail;
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
+  /*  const [isModalVisible, setIsModalVisible] = useState(false);
+   */
+  /*  const showModal = () => {
     setIsModalVisible(true);
-  };
+  }; */
 
   const handleOk = item => {
+    console.log(item, 'aaaaaaaaaa');
     deleteSchedule(item);
 
-    setIsModalVisible(false);
+    /*     setIsModalVisible(false); */
   };
 
-  const handleCancel = () => {
+  /*   const handleCancel = () => {
     setIsModalVisible(false);
   };
-
+ */
   return (
     <div style={{ margin: 20 }}>
       {<CourtScheduleForm court={courtDetail} addSchedule={addSchedule} />}
       <List
         itemLayout='horizontal'
         dataSource={courtSchedule}
-        renderItem={item => (
-          <List.Item
-            style={{ border: 'solid', borderWidth: 0.6, padding: 5, margin: 5 }}
-          >
-            <List.Item.Meta
-              title={<h6>{dayjs(item.day).format('YYYY-MM-DD')}</h6>}
-              description={`${dayjs(item.startTime).format(
-                'HH:mm'
-              )} ==> ${dayjs(item.endTime).format('HH:mm')}\n ${item.price}₮`}
-            />
-            <div>
-              <Button type='primary' onClick={showModal}>
-                Устгах
-              </Button>
-              <Modal
-                title='Цагийн хуваарь устгах'
-                visible={isModalVisible}
-                onOk={() => {
-                  handleOk(item);
-                }}
-                onCancel={handleCancel}
-              >
-                <p>Устгахдаа итгэлтэй байна уу?</p>
-              </Modal>
-            </div>
-          </List.Item>
-        )}
+        renderItem={item => {
+          return (
+            <List.Item
+              style={{
+                border: 'solid',
+                borderWidth: 0.6,
+                padding: 5,
+                margin: 5
+              }}
+            >
+              <List.Item.Meta
+                title={<h6>{dayjs(item.day).format('YYYY-MM-DD')}</h6>}
+                description={`${dayjs(item.startTime).format(
+                  'HH:mm'
+                )} ==> ${dayjs(item.endTime).format('HH:mm')}\n ${item.price}₮`}
+              />
+              <div>
+                <Button type='primary' onClick={() => handleOk(item)}>
+                  Устгах
+                </Button>
+                {/* <Modal
+                  title='Цагийн хуваарь устгах'
+                  visible={isModalVisible}
+                  onOk={() => {
+                    console.log('a');
+                  }}
+                  onCancel={handleCancel}
+                >
+                  <p>Устгахдаа итгэлтэй байна уу?</p>
+                </Modal> */}
+              </div>
+            </List.Item>
+          );
+        }}
       />
     </div>
   );
