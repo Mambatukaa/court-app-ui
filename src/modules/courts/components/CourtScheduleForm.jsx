94088573f814b1c0f9800c761c24df442fc10297
Bat-Amgalan Nasan-Ochir <batamgalan.n@nmma.co>
2021-05-04 01:15:32 +0800

    refactor

--- src/modules/courts/components/CourtScheduleForm.jsx
+++ src/modules/courts/components/CourtScheduleForm.jsx
@@ -57,10 +57,6 @@ const CourtSchedule = props => {
           }}
           layout='horizontal'
         >
-          {/*  <Form.Item label='Өдөр' name='day'>
-            <DatePicker picker='day' />
-          </Form.Item> */}
-
           <Form.Item label='Хугацаа' name='date'>
             <RangePicker
               showTime={{ format: 'HH:mm' }}
